c5ea61dafb5279dfeaec94095cdfc81a23a5cb5b
hhahh2011 <hhahh2011@gmail.com>
2016-05-03 07:40:19 +0800

    message

---
+++ CH.Spartan.Core/Jobs/SendEmailJob.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.BackgroundJobs;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using CH.Spartan.Users;
+
+namespace CH.Spartan.Jobs
+{
+    [Serializable]
+    public class SendEmailJobArgs
+    {
+        public int? TargetTenantId { get; set; }
+
+        public long TargetUserId { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+
+    public class SendEmailJob : BackgroundJob<SendEmailJobArgs>, ITransientDependency
+    {
+        private readonly IRepository<User, long> _userRepository;
+
+        public SendEmailJob(IRepository<User, long> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [UnitOfWork]
+        public override void Execute(SendEmailJobArgs args)
+        {
+            using (CurrentUnitOfWork.DisableFilter(AbpDataFilters.MayHaveTenant))
+            {
+                var user = _userRepository.FirstOrDefault(args.TargetUserId);
+                if (user == null)
+                {
+                    Logger.WarnFormat("Unknown userId: {0}. Can not execute job!", args.TargetUserId);
+                    return;
+                }
+
+                //Here, we should actually send the email! We can inject and use IEmailSender for example.
+                Logger.Info("Sending email to " + user.EmailAddress + " in background job -> " + args.Subject);
+                Logger.Info(args.Body);
+            }
+        }
+    }
+}
---
+++ CH.Spartan.Core/Messages/MessageManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.BackgroundJobs;
+using Abp.Configuration;
+using Abp.Dependency;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Notifications;
+using Abp.Runtime.Caching;
+using Abp.UI;
+using CH.Spartan.Devices;
+using CH.Spartan.Domain;
+using CH.Spartan.Infrastructure;
+using CH.Spartan.Jobs;
+using CH.Spartan.Maps;
+using CH.Spartan.Users;
+
+namespace CH.Spartan.Messages
+{
+    public class MessageManager : ISingletonDependency
+    {
+        private readonly ISettingManager _settingManager;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+        private readonly MapManager _mapManager;
+        private readonly IBackgroundJobManager _backgroundJobManager;
+        private readonly INotificationPublisher _notificationPublisher;
+        private readonly UserManager _userManager;
+
+        public MessageManager(
+            ISettingManager settingManager,
+            IUnitOfWorkManager unitOfWorkManager,
+            MapManager mapManager,
+            IBackgroundJobManager backgroundJobManager,
+            INotificationPublisher notificationPublisher,
+            UserManager userManager)
+        {
+            _settingManager = settingManager;
+            _unitOfWorkManager = unitOfWorkManager;
+            _mapManager = mapManager;
+            _backgroundJobManager = backgroundJobManager;
+            _notificationPublisher = notificationPublisher;
+            _userManager = userManager;
+        }
+
+        [UnitOfWork]
+        public virtual async Task SendMessageAsync(GetwayMessage message)
+        {
+
+            User targetUser;
+            using (_unitOfWorkManager.Current.DisableFilter(AbpDataFilters.MayHaveTenant))
+            {
+                targetUser = await _userManager.FindByIdAsync(message.UserId);
+                if (targetUser == null)
+                {
+                    throw new UserFriendlyException("There is no such a user: " + message.UserId);
+                }
+            }
+
+            //推送Web
+            if (message.Latitude.HasValue && message.Longitude.HasValue)
+            {
+                message.Address =
+                    _mapManager.GetLocation(new MapPoint(message.Latitude.Value, message.Longitude.Value))
+                        .ToString();
+            }
+
+            await
+                _notificationPublisher.PublishAsync(
+                    SpartanConsts.YouHaveAAlarmMessage,
+                    message,
+                    new EntityIdentifier(typeof (Device), message.DeviceId),
+                    message.Severity,
+                    new[] {targetUser.Id},
+                    creatorUserId: 1 //超级管理员
+                    );
+
+            //发送邮件
+            var isSendEmail =
+                await
+                    _settingManager.GetSettingValueForUserAsync<bool>(SpartanSettingKeys.User_IsSendEmail,
+                        targetUser.TenantId, targetUser.Id);
+
+            if (isSendEmail)
+            {
+                await _backgroundJobManager.EnqueueAsync<SendEmailJob, SendEmailJobArgs>(
+                    new SendEmailJobArgs
+                    {
+                        Subject = message.Title,
+                        Body = $"{message.Content} {message.Address}",
+                        TargetTenantId = targetUser.TenantId,
+                        TargetUserId = targetUser.Id
+                    });
+            }
+
+            //推送APP
+            var isSendApp =
+                await
+                    _settingManager.GetSettingValueForUserAsync<bool>(SpartanSettingKeys.User_IsSendApp,
+                        targetUser.TenantId, targetUser.Id);
+
+            if (isSendApp)
+            {
+                await _backgroundJobManager.EnqueueAsync<SendAppJob, SendAppJobArgs>(
+                    new SendAppJobArgs
+                    {
+                        Subject = message.Title,
+                        Body = $"{message.Content} {message.Address}",
+                        TargetTenantId = targetUser.TenantId,
+                        TargetUserId = targetUser.Id
+                    });
+            }
+
+        }
+    }
+}
